9fc760848fc5f996570d08e12957eefb4e8bc2fc
Antoine Renaud <renaud_e@epita.fr>
2016-04-13 01:19:21 +0200

    Binouze

--- Code/Affichage/CharCut.c
+++ Code/Affichage/CharCut.c
@@ -1,9 +1,11 @@
 #include <SDL.h>
 #include <stdlib.h>
 # include "CharCut.h"
-# include "Char.h"
-# include "Char.c"
+
+
 # include "pixel_operations.h"
+# include "sdl.h"
+
 void Line(SDL_Surface *surf)
 {
   //int Xmin= 0;
@@ -69,4 +71,61 @@ void SavePartScreen(const char* nom, SDL_Surface *ecran, int Xmin, int Ymin, int
   SDL_SaveBMP(image, nom);
   SDL_FreeSurface(image);
 }*/
+
+void Char(SDL_Surface *surf, int Xmin, int Ymin, int haut, int larg){
+ int booleen =  0;
+ int booleen2 = 0;
+ Uint8 r = 0;
+ Uint8 g = 0;
+ Uint8 b = 0;
+ Uint32 pixel;
+ int test2nd = 0;
+ for(int x = 0; x < surf->w; x++)
+  {
+    for(int y= Ymin; y < haut; y++)
+    {
+      pixel = getpixel(surf, x, y);
+      SDL_GetRGB(pixel, surf->format, &r, &g, &b);
+      if(r==0 && g==0 && b==0) /* Si c'est noir */
+      {
+        booleen  = 1;
+        booleen2 = 0;
+      }
+    }
+    if(booleen == 0) /*check si c'est une colonne blanche, sinon ça part*/
+    {
+      if(booleen2 == 1)
+      {
+        larg = 0;
+        Xmin = x + 1;
+      }
+      if(booleen2 == 0)
+      {
+        test2nd++;
+        if(test2nd == 2){
+           neurone(surf, Xmin, Ymin, haut, larg);
+        }
+        Xmin = x + 1;
+        larg = 0;
+        booleen2 = 1;
+      }
+    }
+    larg = larg + 1;
+    booleen = 0;
+  }
+}
+
+void neurone(SDL_Surface *surf, int Xmin, int Ymin, int haut, int larg){
+  SDL_Rect OneChar;
+  OneChar.x = Xmin;
+  OneChar.y = Ymin;
+  OneChar.w = larg;
+  OneChar.h = haut;
+  SDL_Surface disp;
+  SDL_BlitSurface(surf, &OneChar, &disp, NULL);
+  display_image(&disp);
+}
+
+
+
 
--- Code/Affichage/CharCut.h
+++ Code/Affichage/CharCut.h
@@ -13,5 +13,9 @@
 
 Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y);
 void Line(SDL_Surface *surf);
+void Char(SDL_Surface *surf, int Xmin, int Ymin, int haut, int larg);
+void neurone(SDL_Surface *surf, int Xmin, int Ymin, int haut, int larg);
+
+
 # endif
 
