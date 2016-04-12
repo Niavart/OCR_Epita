18aecbd1effb283ca5af5bc5d8dd95ec415012c2
Antoine Renaud <renaud_e@epita.fr>
2016-04-12 23:57:56 +0200

    Some errors left with undefined fucntion

---
+++ Code/Affichage/Char.h
@@ -0,0 +1,18 @@
+// Char.h
+//vieux copié collé de base du pixel_operation.h
+
+# ifndef CHAR_H_
+# define CHAR_H_
+
+
+# include <stdlib.h>
+# include <SDL.h>
+# include <stdlib.h>
+# include "pixel_operations.h"
+
+void Char(SDL_Surface *surf, int Xmin, int Ymin, int haut, int larg);
+
+void neurone(SDL_Surface *surf, int Xmin, int Ymin, int haut, int larg);
+
+# endif
+           
---
+++ Code/Affichage/CharCut.h
@@ -0,0 +1,17 @@
+// CharCut.h
+//vieux copié collé de base du Char.h
+
+# ifndef CHARCUT_H_
+# define CHARCUT_H_
+
+
+# include <stdlib.h>
+# include <SDL.h>
+# include <stdlib.h>
+# include "pixel_operations.h"
+
+
+Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y);
+void Line(SDL_Surface *surf);
+# endif
+
