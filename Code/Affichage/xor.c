1a38efbfc606095d03ebe2196f973d6a514fd2ab
Hyuujin-Scarecrow <scare96lamarmotte@gmail.com>
2016-04-12 22:36:45 +0200

    blablabla

--- Code/Affichage/xor.c
+++ Code/Affichage/xor.c
@@ -13,7 +13,7 @@ float bz;	//biais de z
 
 float XORtab[4][3];
 
-float init() // initialise le reseau (avant apprentissage)
+void init() // initialise le reseau (avant apprentissage)
 {
 	int i = 0;
 	int j = 0;
@@ -24,27 +24,28 @@ float init() // initialise le reseau (avant apprentissage)
 			poids[i][j] = 0;
 			j++;
 		}
-	i++;
-	j=0;
+		i++;
+		j=0;
 	}
-x=1;
-y=0;
-h=0.5;
-z=0.5
-bz=1;
-bh=1;
-XORtab[0][0] = 0;
-XORtab[0][1] = 0;
-XORtab[0][2] = 0;
-XORtab[1][0] = 0;
-XORtab[1][1] = 1;
-XORtab[1][2] = 1;
-XORtab[2][0] = 1;
-XORtab[2][1] = 0;
-XORtab[2][2] = 1;
-XORtab[3][0] = 1;
-XORtab[3][1] = 1;
-XORtab[3][2] = 0;
+
+	x=1;
+	y=0;
+	h=0.5;
+	z=0.5;
+	bz=1;
+	bh=1;
+	XORtab[0][0] = 0;
+	XORtab[0][1] = 0;
+	XORtab[0][2] = 0;
+	XORtab[1][0] = 0;
+	XORtab[1][1] = 1;
+	XORtab[1][2] = 1;
+	XORtab[2][0] = 1;
+	XORtab[2][1] = 0;
+	XORtab[2][2] = 1;
+	XORtab[3][0] = 1;
+	XORtab[3][1] = 1;
+	XORtab[3][2] = 0;
 
 
 }
@@ -60,10 +61,10 @@ float ns(float a, float b) //calcule la valeur de sortie
 	return(1/(1+exp((a*(poids[1][6]))+(b*(poids[2][6]))+(nint*(poids[4][6]))+(bz*(poids[5][6])))));
 }
 
+
 float calcerreur(float a, float b) // calcule l'erreur en fonction de x et de y
 {
-	float nint = nc(a, b);
-	float ok = ns(a, b, nint);
+	float ok = ns(a, b);
 	float der = ok*(1-ok);
 	if(a != b)
 	{
@@ -76,29 +77,12 @@ float calcerreur(float a, float b) // calcule l'erreur en fonction de x et de y
 
 }
 
-float changerpoids(float sortie, float n)
-{
-	
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 int main()
 {
-	
-	
-	
+	return 0;
 }
+
+
+
+
