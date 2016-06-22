8e6127865d08614e06d97904f99be050571eefde
Antoine Renaud <renaud_e@epita.fr>
2016-06-22 22:16:02 +0200

    press OK for Line(). Press dilate for Segmentation Fault

--- Code/Affichage/Interface.c
+++ Code/Affichage/Interface.c
@@ -1,18 +1,23 @@
 #include <stdlib.h>
 #include <gtk-2.0/gtk/gtk.h>
 #include "sdl.c"
+#include <libgen.h>
 void OnToggle();
 void Selection();
-void Chemin(GtkWidget *file_selection);
+void dilate();
+void Chemin (GtkWidget *widget, gpointer user_data);
+GtkWidget *pFilename = NULL;
+SDL_Surface *img = NULL;
 
 int main(int argc, char **argv)
 {
     GtkWidget *MainWindow = NULL;
     GtkWidget *OKButton = NULL;
+    GtkWidget *dilate = NULL;
     GtkWidget *ChooseFile = NULL;
     GtkWidget *Quitter = NULL;
     GtkWidget *pVBox = NULL;
-    GtkWidget *pFilename = NULL;
+//  GtkWidget *pFilename = NULL;
     gchar *Filename = g_locale_to_utf8("Aucun fichier pour le moment", -1, NULL, NULL, NULL);
     gtk_init(&argc, &argv);
 
@@ -41,23 +46,31 @@ int main(int argc, char **argv)
     /* Création du bouton OK*/
     OKButton = gtk_toggle_button_new_with_label("OK");
     gtk_box_pack_start(GTK_BOX(pVBox), OKButton, FALSE, FALSE, 0);
-    
+   
+    /* Création du bouton test dilate_pixel*/
+    dilate = gtk_toggle_button_new_with_label("dilate_pixel");
+    gtk_box_pack_start(GTK_BOX(pVBox), dilate, FALSE, FALSE, 0);
+
     gtk_box_pack_start(GTK_BOX(pVBox), Quitter, FALSE, FALSE, 0);
     gtk_widget_show_all(MainWindow);
 
     /*g_signal_connect(G_OBJECT(ChooseFile), "toggled", G_CALLBACK(OnToggle), NULL);*/
     g_signal_connect(G_OBJECT(ChooseFile), "clicked", G_CALLBACK(Selection), NULL);
     g_signal_connect(G_OBJECT(OKButton), "toggled", G_CALLBACK(OnToggle), NULL);
+    g_signal_connect(G_OBJECT(dilate), "toggled", G_CALLBACK(dilate), NULL);
     g_signal_connect(G_OBJECT(Quitter), "toggled", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(G_OBJECT(MainWindow), "delete-event", G_CALLBACK(gtk_main_quit), NULL);
 
     gtk_main();
     return EXIT_SUCCESS;
 }
-
+void dilate(){
+	dilate_pixel(img);
+}
 void OnToggle()
-{
-    gtk_main_quit();
+{	
+	Line(img);
+	//gtk_main_quit();
 
 }
 
@@ -71,17 +84,30 @@ void Selection()
     g_signal_connect_swapped(G_OBJECT(GTK_FILE_SELECTION(pSelection)->cancel_button), "clicked", G_CALLBACK(gtk_widget_destroy), pSelection);
 }
 
-void Chemin(GtkWidget *file_selection)
+void Chemin(GtkWidget *widget, gpointer user_data)
 {
-    const gchar *chemin;
-    GtkWidget *dialog;
-    chemin = gtk_file_selection_get_filename(GTK_FILE_SELECTION(file_selection));
-    dialog = gtk_message_dialog_new(GTK_WINDOW(file_selection),
-    GTK_DIALOG_MODAL,
-    GTK_MESSAGE_INFO,
-    GTK_BUTTONS_OK,
-    "You choose :\n%s", chemin);
-    gtk_dialog_run(GTK_DIALOG(dialog));
-    gtk_widget_destroy(dialog);
-    gtk_widget_destroy(file_selection);
+	GtkWidget *file_selector = GTK_WIDGET (user_data); 
+	const gchar *selected_filename; 
+	selected_filename = gtk_file_selection_get_filename (GTK_FILE_SELECTION (file_selector));
+	char *merde = (char*)selected_filename;
+	widget = widget +1 -1;
+	g_print ("Selected filename: %s\n", selected_filename);
+//	char *pfile;
+	char *name = merde;
+	name = basename(name);
+/*    	pfile = name[0] + strlen(name[0]);
+   	for (; pfile > name[0]; pfile--)
+   	{
+       	    if ((*pfile == '\\') || (*pfile == '/'))
+            {
+           	 pfile++;
+          	 break;
+      	    }
+ 	}*/
+//	char *name = &merde;
+	printf(" \n name is equal to %c", *name);
+	img = load_image(name);
+	
+
+        gtk_widget_destroy(file_selector);
 }
